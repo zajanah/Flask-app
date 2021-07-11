339475ba4498fe20336b205f45519c8ccea6ef8a
Your Name <yassirroney@gmail.com>
2021-07-11 13:57:31 +0100

    flask app

--- app.py
+++ app.py
@@ -1,56 +1,82 @@
-from flask import Flask, render_template, request
-import jsonify
-import requests
 import pickle
-import numpy as np
-import sklearn
-import matplotlib
+
+from flask import Flask, render_template, request
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from imblearn.combine import SMOTETomek
+
 app = Flask(__name__)
-model = pickle.load(open('Customer_Churn_Prediction.pkl', 'rb'))
+
+# Splitting the dataset into the Training set and Test set
+x_train, x_test, y_train, y_test = train_test_split('X.csv', 'Y.csv', test_size=0.2, random_state=0)
+
+# Scaling data
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+# Oversample data
+smk = SMOTETomek()
+# Training data
+x_train, y_train = smk.fit_sample(x_train, y_train)
+# Testing data
+x_test, y_test = smk.fit_sample(x_test, y_test)
+
+# Fitting RandomForestClassifier to the Training set
+rfr = RandomForestClassifier()
+rfr.fit(x_train, x_train)
+
+# Predicting the Test set results
+y_pred = rfr.predict(x_test)
+
+# Saving model to disk
+pickle.dump(rfr, open('model.pkl', 'wb'))
+
+# Loading model to compare the results
+model = pickle.load(open('model.pkl', 'rb'))
+
+
 @app.route('/', methods=['GET'])
-def Home():
+def home():
     return render_template('index.html')
 
+
 standard_to = StandardScaler()
-@app.route('/predict',methods=['POST'])
+
+
+@app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        CreditScore = int(request.form['CreditScore'])
-        Age = int(request.form['Age'])
-        Tenure = int(request.form['Tenure'])
-        Balance = float(request.form['Balance'])
-        NumOfProducts = int(request.form['NumOfProducts'])
-        HasCrCard = int(request.form['HasCrCard'])
-        IsActiveMember = int(request.form['IsActiveMember'])
-        EstimatedSalary = float(request.form['EstimatedSalary'])
-        Geography_Germany = request.form['Geography_Germany']
-        if(Geography_Germany == 'Germany'):
-            Geography_Germany = 1
-            Geography_Spain= 0
-            Geography_France = 0
-                
-        elif(Geography_Germany == 'Spain'):
-            Geography_Germany = 0
-            Geography_Spain= 1
-            Geography_France = 0
-        
+        credit_score = int(request.form['CreditScore'])
+        age = int(request.form['Age'])
+        tenure = int(request.form['Tenure'])
+        balance = float(request.form['Balance'])
+        num_of_products = int(request.form['NumOfProducts'])
+        has_cr_card = int(request.form['HasCrCard'])
+        is_active_member = int(request.form['IsActiveMember'])
+        estimated_salary = float(request.form['EstimatedSalary'])
+        geography = request.form['Geography']
+        if geography == 'Germany':
+            geography = 1
+
+        elif geography == 'Spain':
+            geography = 2
+
         else:
-            Geography_Germany = 0
-            Geography_Spain= 0
-            Geography_France = 1
-        Gender_Male = request.form['Gender_Male']
-        if(Gender_Male == 'Male'):
-            Gender_Male = 1
-            Gender_Female = 0
+            geography = 0
+        gender = request.form['Gender']
+        if gender == 'Male':
+            gender = 0
         else:
-            Gender_Male = 0
-            Gender_Female = 1
-        prediction = model.predict([[CreditScore,Age,Tenure,Balance,NumOfProducts,HasCrCard,IsActiveMember,EstimatedSalary,Geography_Germany,Geography_Spain,Gender_Male]])
-        if prediction==1:
-             return render_template('index.html',prediction_text="The Customer will leave the bank")
+            gender = 1
+        prediction = model.predict([[credit_score, age, tenure, balance, num_of_products, has_cr_card, is_active_member,
+                                     estimated_salary, geography, gender]])
+        if prediction == 1:
+            return render_template('index.html', prediction_text="The Customer will leave the bank")
         else:
-             return render_template('index.html',prediction_text="The Customer will not leave the bank")
-                
-if __name__=="__main__":
+            return render_template('index.html', prediction_text="The Customer will not leave the bank")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
